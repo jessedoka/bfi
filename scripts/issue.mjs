a6d78ee3f17e000a5e4c89b7ddd91788ba2d4e6b
j-doka <jdoka42@gmail.com>
2023-06-16 23:40:41 +0100

    starter functions complete

--- scripts/issue.mjs
+++ scripts/issue.mjs
@@ -10,3 +10,44 @@ const octokit = new Octokit({
     auth: token,
 });
 
+let seed = [
+    {
+        "owner": "facebook",
+        "repo": "react",
+    }
+]
+
+const getIssues = async (owner, repo) => {
+    const issues = await octokit.issues.listForRepo({
+        owner: owner,
+        repo: repo,
+    });
+    return issues;
+}
+
+const getIssuesForAllRepos = async (seed) => {
+    let issues = [];
+    for (let i = 0; i < seed.length; i++) {
+        const { owner, repo } = seed[i];
+        const issuesForRepo = await getIssues(owner, repo);
+        issues.push(issuesForRepo);
+    }
+    return issues;
+}
+
+const getUserfromIssue = (issue) => {
+    return issue.user.login;
+}
+
+const getAllReposfromUser = async (user) => {
+    const repos = await octokit.repos.listForUser({
+        username: user,
+    });
+    return repos;
+}
+
+const main = async () => {
+
+}
+
+main();
